d2bd60d259534ede3daee3b95a536561447b9081
FerusGrim <ferusgrim@gmail.com>
2014-04-03 19:46:11 -0400

    Executor Update
    New method for Executor that's pretty smooth.

--- src/io/github/ferusgrim/GrimBanned/Executor.java
+++ src/io/github/ferusgrim/GrimBanned/Executor.java
@@ -1,18 +1,14 @@
 package io.github.ferusgrim.GrimBanned;
 
-import java.util.logging.Level;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 /**
  * Copyright (C) 2014 Nicholas Badger
  * @author FerusGrim
  */
 
-@SuppressWarnings("unused")
 public class Executor implements CommandExecutor {
 	private GrimBanned plugin;
 
@@ -23,27 +19,28 @@ public class Executor implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		String command = cmd.getName().toLowerCase();
-		boolean hasPerm = false;
-		if(!(sender instanceof Player))hasPerm = true;
-		if(!hasPerm){
-			Player player = null;
-			player = (Player) sender;
-			if(player.hasPermission("grimbanned." + command))hasPerm = true;
-			if(!hasPerm){
-				sender.sendMessage("[GB] Insufficient Permissions!"); 
-				return true;
-			}
+		if(!sender.hasPermission("grimbanned." + command)){
+			sender.sendMessage("[GB] Sorry, but you have insufficient permissions to run this command!");
+			return false;
 		}
 		if(args.length < 1 && !command.equals("kickall")){
 			return false;
 		}
-		if(command.equals("ban"))return BanManager.Ban(plugin, sender, args);
-		if(command.equals("banip"))return BanManager.BanIp(plugin, sender, args);
-		if(command.equals("unban"))return BanManager.Unban(sender, args);
-		if(command.equals("unbanip"))return BanManager.UnbanIp(sender, args);
-		if(command.equals("kick"))return KickManager.Kick(plugin, sender, args);
-		if(command.equals("kickall"))return KickManager.KickAll(plugin, sender, args);
-		return false;
+		switch(command){
+			case "ban":
+				return BanManager.Ban(plugin, sender, args);
+			case "banip":
+				return BanManager.BanIp(plugin, sender, args);
+			case "unban":
+				return BanManager.Unban(sender, args);
+			case "unbanip":
+				return BanManager.UnbanIp(sender, args);
+			case "kick":
+				return KickManager.Kick(plugin, sender, args);
+			case "kickall":
+				return KickManager.KickAll(plugin, sender, args);
+			default:
+				throw new IllegalArgumentException("[GB] Command is unknown!: " + label);
+		}
 	}
-
 }
